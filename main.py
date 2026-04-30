6b2c1fbb357e622e7e6c7c3488a9338ce47672a5
incmach <incmach@gmail.com>
2026-04-30 17:19:35 +0300

    Erroneous implementation of the simplest ntt algorithm

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import functools as ft
 import itertools as it
 import math
 import time
@@ -7,17 +8,25 @@ import galois
 
 TEST = True
 
-#TODO a couple of tests for it
 #TODO joblib
 def compute_spectrum_naive(I):
-    result = 2*math.prod(I.shape)*[0]
+    result = 2*math.prod(I.shape)*[int(0)]
     for vs in it.product(it.product(*[range(n) for n in I.shape]), repeat = len(I.shape) + 1):
         signed_area = int(np.round(np.linalg.det(
             [ [ vs[i+1][j] - vs[0][j] for j in range(len(I.shape)) ] for i in range(len(I.shape)) ])))
+        if signed_area == 0:
+            continue
         result[signed_area] += math.prod(int(I[v]) for v in vs)
     for i, v in enumerate(result):
         result[i] = v//3
-    return result[:math.prod(I.shape)]
+    if result:
+        result = result[:math.prod(I.shape)]
+        result[0] = math.comb(sum(sum(int(v) for v in row) for row in I), 3) - sum(result[1:])
+    *_, gcd = it.accumulate(result[1:], galois.gcd, initial = 0)
+    if gcd > 0:
+        print(gcd)
+        print([v//gcd for v in result])
+    return result
         
 if TEST:
     assert(compute_spectrum_naive(np.zeros((0, 0), dtype = np.uint8)) == [ ])
@@ -40,52 +49,49 @@ if TEST:
         [      0,      0,     16,     12,     17,      4],
         [      0,      0,      0,     19,     13,      9],
         [      0,      0,      0,      0,2**32+1,      0]], dtype=np.uint64))
-    print(stretched_as[::2])
-    print(unstretched_as)
-    assert(stretched_as[::2] == unstretched_as[:len(stretched_as)//2])
-    assert(unstretched_as[len(stretched_as)//2:] == (len(unstretched_as)-len(stretched_as)//2)*[0])
-def compute_max_spectrum(image_shape, spectrum_size, max_pixel_value):
-    m, n = image_shape
-    I = np.ones(image_shape, dtype = np.uint8)
-    N = 2*spectrum_size
-    S = np.zeros((N,), dtype = np.uint64)
-    for y1, x1, y2, x2, y3, x3 in itertools.product(range(m), range(n), repeat = 3):
-        if y2 == x2 == x3 == y3 == 0:
-            print(f'{x1}x{y1}')
-        S[x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)] += np.uint64(I[y1,x1])*I[y2,x2]*I[y3,x3]
-    S //= 6
-    S *= max_pixel_value**3
-    return S
- 
-if __name__ == "__main__" and not TEST:
-    image_shape = (16, 32)
-    spectrum_size = math.prod(image_shape)
-    max_pixel_value = 255
-    # we should later take spectrum[1:] to exclude 0s item which is trivially computed from other elements
-    p = int(np.max(compute_max_spectrum(image_shape, spectrum_size, max_pixel_value)))
+    assert(len(stretched_as) == len(unstretched_as))
+    for i in range(1, len(unstretched_as)):
+        if i*2 < len(stretched_as):
+            assert(stretched_as[i*2] == unstretched_as[i])
+        else:
+            assert(unstretched_as[i] == 0)
+        if i%2 == 1:
+            assert(stretched_as[i] == 0)
+
+def compute_max_spectrum(image_shape, max_pixel_value):
+    multiplier = max_pixel_value**3
+    result = [ multiplier*v for v in compute_spectrum_naive(np.ones(image_shape, dtype = np.uint8)) ]
+    return result
+
+if TEST: 
+    assert(compute_max_spectrum((4,4), 255)[1:] == compute_spectrum_naive(255*np.ones((4,4), dtype=np.uint8))[1:])
+
+def compute_area_spectrum_ntt_simple(I, p):
+    # 1. Compute ntt per row
+    spectrum_size = math.prod(I.shape)
+    double_spectrum_size = 2*spectrum_size
     while True:
         p = galois.next_prime(p)
         if (p-1)%(2*spectrum_size)== 0:
             break 
         p += 1
-    print(f'p == 2^{math.log2(p):.2f}')
     GF = galois.GF(p)
-    J = np.random.randint(0, max_pixel_value+1, size = image_shape, dtype = np.uint8)
-    J = GF(J)
-    NTTJT = GF([galois.ntt(J[r], 2*spectrum_size) for r in range(image_shape[0])]).T
-
-    NTTAS = GF(np.zeros(2*spectrum_size, dtype=np.uint8))
-    # iterate through triplets of rows
-    for ys in itertools.product(range(image_shape[0]), repeat = 3):
+    rows = I.shape[0]
+    NTT_I = GF([ galois.ntt(GF(I[r]), double_spectrum_size) for r in range(I.shape[0]) ]).T
+    NTT_R = GF(np.zeros(double_spectrum_size, dtype=np.uint8))
+    for ys in it.product(range(rows), repeat = 3):
         if ys[1] == ys[2] == 0:
             print(f'starting {ys[0]}')
         complement = [
-                NTTJT[ [ k*(ys[(y_i+1)%3]-ys[(y_i+2)%3])%(spectrum_size*2) for k in range(spectrum_size*2) ], y_i ]
+                NTT_I[ [ k*(ys[(y_i+1)%3]-ys[(y_i+2)%3])%(spectrum_size*2) for k in range(double_spectrum_size) ], y_i ]
                 for y_i in range(3)
             ]
-        NTTAS += GF(1)/GF(6)*math.prod(complement)
-    AS = galois.intt(NTTAS)
-    print(AS)
+        NTT_R += GF(1)/GF(3)*math.prod(complement)
+    return [ int(v) for v in galois.intt(NTT_R)[:spectrum_size] ]
 
-    print(f'done');
-
+if TEST:
+    I = np.random.randint(0, 16, size = (5,5), dtype = np.uint8)
+    reference = compute_spectrum_naive(I)
+    print(reference)
+    computed = compute_area_spectrum_ntt_simple(I, max(reference[1:])+1)
+    print(computed)
